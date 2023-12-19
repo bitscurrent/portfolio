94604a81da3d8222c132067dbcd315df2f4e86d1
Dilip <dilipboidya.office@gmail.com>
2023-12-19 14:21:58 +0530

    unnecessary features removed

--- main.js
+++ main.js
@@ -17,37 +17,6 @@ function opentab(tabname) {
 
 
 
-
-    
-// Check if dark mode is enabled in local storage
-const darkModeEnabled = localStorage.getItem('darkMode') === 'enabled';
-
-// Apply dark mode styles if enabled
-if (darkModeEnabled) {
-    document.body.classList.add('dark-mode');
-}
-
-// Toggle dark mode when the button is clicked
-let darkMode = document.getElementById('dark-mode-toggle')
-darkMode.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-
-    // Save the user's preference in local storage
-    if (document.body.classList.contains('dark-mode')) {
-        localStorage.setItem('darkMode', 'enabled');
-        darkMode.textContent = darkMode.textContent ==  "Dark" ? "Light" : "Dark";
-    } 
-    
-    else {
-        localStorage.setItem('darkMode', 'disabled');
-    }
-});
-
-
-
-
-
-
 let sidemenu = document.getElementById("sidemenu");
 
 function openmenu(){
